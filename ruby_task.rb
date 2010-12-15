4d5e876843d55f95de9fa286b29a7909bc07fbcf
Ernest <ernest.sankeshwar@gmail.com>
2010-12-15 16:46:06 +0530

    completed pdf support

--- ruby_task.rb
+++ ruby_task.rb
@@ -1,7 +1,12 @@
+require 'rubygems'
+require 'pdf/toolkit'
+require 'pdf/writer'
+
 class ResumeDetails
   attr_reader :name
   attr_reader :age
   attr_reader :address
+  attr_reader :file_type
   
   def name=(new_name)
     @name = new_name
@@ -14,18 +19,24 @@ class ResumeDetails
   def address=(new_address)
     @address = new_address
   end
-  
-  def initialize(name, age, address)
+
+  def file_type=(new_file_type)
+    @file_type = new_file_type
+  end
+    
+  def initialize(name, age, address , file_type ="txt" )
     @name = name
     @age = age.to_i
     @address = address
+    @file_type = file_type
   end
   
   def validate
  #   begin
-      raise "invalid name" if( (@name.size ==0) || (@name.match(/([^\w])+/)) )
+      raise "invalid name" if( (@name.size ==0) || (@name.match(/([^\w\s])+/)) )
       raise "invalid age" if(  (@age == 0)    ||  (@age.to_s.match(/([^\d])+/) ) )
-      raise "invalid address" if( (@address.size == 0) || (@address.match(/([^\w\d])+/) ))
+      raise "invalid address" if( (@address.size == 0) || (@address.match(/([^\w\d\s])+/) ))
+      raise "invalid file type" if( (@file_type.size == 0) || (@file_type.match(/[^txt|pdf]/) ) )
 #    rescue Exception => e
 #      puts e.to_s
  #   end
@@ -33,6 +44,25 @@ class ResumeDetails
 end
 
 class InputOutput
+  def write_to_pdf(file_name,file_entry)
+    resume = PDF::Toolkit.open(file_name)
+    resume_file_text = resume.to_text.read
+    resume_file_text = resume_file_text+file_entry
+    resume = PDF::Writer.new()
+    resume.text(resume_file_text)
+    resume_file_stream = resume.render()
+    File.open(file_name , "w") do |file|
+      file.puts resume_file_stream
+    end
+  end
+
+  def write_to_txt(file_name,file_entry)
+    File.open(file_name,"a") do |file|
+      file.puts file_entry
+    end
+  end
+  
+  
   def read_from_console(obj=STDIN)
     entered_string = obj.gets.chomp
     return entered_string
@@ -42,15 +72,16 @@ class InputOutput
     rd.validate()
     file_entry = "#{rd.name},#{rd.age},#{rd.address}"
 
-    File.open("resume_details.txt","a") do |file|
-      file.puts file_entry
+    if(rd.file_type == "pdf") :
+      write_to_pdf("resume_details.pdf",file_entry)  
+    else
+      write_to_txt("resume_details.txt",file_entry)
     end  
   end
-  
 end
 
 
-=begin
+#=begin
 class MainClass
 
   io =InputOutput.new
@@ -61,9 +92,11 @@ class MainClass
   age = io.read_from_console()
   puts "Enter The Address"
   address =io.read_from_console()
+  puts "Enter the file type of the resume 'txt' for a text file and 'pdf' for a odf file"
+  file_type = io.read_from_console()
 
-  rd = ResumeDetails.new(name,age,address)
+  rd = ResumeDetails.new(name,age,address,file_type)
   io.write_to_file(rd)
   puts "Data Written to file"
 end
-=end
+#=end
--- test.rb
+++ test.rb
@@ -1,29 +1,33 @@
 require 'ruby_task'
 require 'test/unit'
+require 'rubygems'
+require 'pdf/toolkit'
 
 class TestResume < Test::Unit::TestCase
 
   def test_acessor_methods
-    rd = ResumeDetails.new("ernest",20,"dharwad")
+    rd = ResumeDetails.new("ernest",20,"dharwad","txt")
     rd.name = "aditya"
     rd.age = "12"
     rd.address = "hubli"
+    rd.file_type = "pdf"
     assert_equal("aditya" , rd.name,"name not same")
     assert_equal(12 , rd.age, "age not same")
     assert_equal("hubli" ,rd.address,"address not same")
+    assert_equal("pdf",rd.file_type, "filetype is not the same")
   end
   
   
   def test_valid_file_write
     io = InputOutput.new
-    rd = ResumeDetails.new("ernest",22,"dharwad")
+    rd = ResumeDetails.new("ernest",22,"dharwad","txt")
     assert_nothing_raised() {io.write_to_file(rd)}
   end
   
   def test_invalid_file_write
     io = InputOutput.new
-    rd = ResumeDetails.new("",0,"")
-    rd1 = ResumeDetails.new("aditya",123,"")
+    rd = ResumeDetails.new("",0,"","doc")
+    rd1 = ResumeDetails.new("aditya",123,"","xsl")
     assert_raise(RuntimeError) {io.write_to_file(rd)}
     assert_raise(RuntimeError) {io.write_to_file(rd1)}
   end
@@ -33,20 +37,28 @@ class TestResume < Test::Unit::TestCase
   end
   
   def test_initalized_instance
-    rd = ResumeDetails.new("ernest","22","dharwad")
+    rd = ResumeDetails.new("ernest","22","dharwad","txt")
     assert_equal("ernest",rd.name,"name is not as expected")
     assert_equal(22 , rd.age ,"age is not as expected")
     assert_equal("dharwad" , rd.address,"address is not as expected")
+    assert_equal("txt" , rd.file_type , "filetype is not initailized as expected")
   end
   
   def test_wirte_to_file
+    rd = ResumeDetails.new("abc",12,"qwe","txt")
     file_text = ""
-    File.open("resume_details.txt") do|file|
-      while line = file.gets
-        file_text += line
+    file_name = "resume_details."+rd.file_type
+    if(rd.file_type == "pdf") :
+      my_file = PDF::Toolkit.open(file_name)
+      file_text = my_file.to_text.read
+    else
+      File.open(file_name) do|file|
+        while line = file.gets
+          file_text += line
+        end
       end
     end
-    assert_not_equal(0 , file_text.size,"the file is empty")
+     assert_not_equal(0 , file_text.size,"the file is empty")
   end
   
   def test_read_from_console
